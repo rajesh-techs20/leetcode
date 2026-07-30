be3a2ec96ea99dae6ea5ba5463583a2d9fabd64a
RAJESH G R <rgr98624@gmail.com>
2026-07-30 23:02:35 +0530

    Update 183.sql

--- 183.sql
+++ 183.sql
@@ -1,1 +1,1 @@
-SELECT name AS Customers FROM Customers LEFT JOIN Orders ON Customer.id=Orders.CustomerID WHERE ORDERS.CustomerID is NULL;
+SELECT name AS Customers FROM Customers LEFT JOIN Orders ON Customer.id=Orders.CustomerID WHERE Orders.CustomerID is NULL;
